edbe8e1a0556f0a1517bd7750cc4ab5da70559c6
Sushan <85663813+Sushan2589@users.noreply.github.com>
2025-07-25 18:28:45 +0545

    Small change

--- app/api/participants/route.js
+++ app/api/participants/route.js
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server";
 import supabase from "@/app/config/supabase";
 
 export async function GET() {
-  console.log("SUPABASE_SERVICE_ROLE_KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY ? "SET" : "NOT SET");
-console.log("NEXT_PUBLIC_SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL ? "SET" : "NOT SET");
-
+ 
   const { data, error } = await supabase.from("participants").select("*");
 
   if (error)
